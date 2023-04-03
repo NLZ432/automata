d69649ff73ea5f29321cabcfd95fec3093147c02
Nestor <nlzx432@gmail.com>
2023-04-02 23:05:08 -0400

    fix preset priorities

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
   const grid = new HyperGrid(Random1, 50); 
 
   // pretty presets :3
-  grid.addWanderingZone(new WanderingZone(UlamWarburton, 5, 25, 25, 1, 20))
-  grid.addWanderingZone(new WanderingZone(ConwayLife, 20, 25, 25, 2, 25))
+  grid.addWanderingZone(new WanderingZone(UlamWarburton, 5, 25, 25, 2, 20))
+  grid.addWanderingZone(new WanderingZone(ConwayLife, 20, 25, 25, 1, 25))
 
   grid.setRunning(true);
 
