4ec95fb7f4e009ea71a4f89c1300a60e46dba868
Nestor Lopez <nlzx432@gmail.com>
2025-04-16 21:10:59 -0400

    control panel floats above the grid

--- src/components/PlayButton/PlayButton.tsx
+++ src/components/PlayButton/PlayButton.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import '../../App.css';
 
 export default function PlayButton(props: { running: boolean, setRunning: (val: boolean) => void }) {
@@ -6,8 +6,8 @@ export default function PlayButton(props: { running: boolean, setRunning: (val: 
         props.setRunning(!props.running);
     }
     return (
-        <button className="Playbutton" onClick={handleClick}>
-            <p> { props.running ? "Pause" : "Play" } </p>
+        <button className="Playbutton" onClick={handleClick} style={{minWidth: '80px', height: '36px', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+            <p style={{margin: 0}}>{ props.running ? "Pause" : "Play" }</p>
         </button>
     );
 }
